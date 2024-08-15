f62cc9d4e992dbdb7a77bd60e332d8be8baa4358
Abdulazeez Olalere <olalereazeez11@gmail.com>
2024-08-15 18:08:19 +0100

    FEAT: setup nativewind

---
+++ app/index.tsx
@@ -0,0 +1,40 @@
+import {
+  DarkTheme,
+  DefaultTheme,
+  ThemeProvider,
+} from "@react-navigation/native";
+import { useFonts } from "expo-font";
+import { Stack } from "expo-router";
+import * as SplashScreen from "expo-splash-screen";
+import { useEffect } from "react";
+import "react-native-reanimated";
+
+import { useColorScheme } from "@/hooks/useColorScheme";
+import { Text, View } from "react-native";
+import { StatusBar } from "expo-status-bar";
+
+// Prevent the splash screen from auto-hiding before asset loading is complete.
+SplashScreen.preventAutoHideAsync();
+
+export default function RootLayout() {
+  const colorScheme = useColorScheme();
+  const [loaded] = useFonts({
+    SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
+  });
+
+  useEffect(() => {
+    if (loaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded]);
+
+  if (!loaded) {
+    return null;
+  }
+
+  return (
+    <View className="flex-1 items-center justify-center">
+      <Text>Hello world You</Text>
+    </View>
+  );
+}
